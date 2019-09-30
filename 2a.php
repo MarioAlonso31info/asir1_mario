3cebb6caa3ec357875580048e963e4d5bb731fcb
Mario Alonso Gutiérrez <marrioalonso31info@gmail.com>
2019-09-30 11:02:52 +0200

    Prueba variable

---
+++ 2a.php
@@ -0,0 +1,7 @@
+<?php
+$a =5;
+$b =3;
+echo ($a + $b)."<br>";
+echo ($a - $b)."<br>";
+echo ($a * $b)."<br>";
+echo ($a / $b)."<br>";
--- asir1_1.php
+++ asir1_1.php
@@ -1,8 +1,12 @@
+<a href="http://192.168.0.96/asir1_adrian/Asir1_i.php">Adrian Villar</a>
 <p>Mario</p>
-<a href="http://192.168.0.93/asir1_bruno">Bruno Lamadrid</a>
+<a href="http://192.168.0.93/asir1_bruno/asir1bruno.php">Bruno Lamadrid</a>
+<br>
+<br>
+
 <?php
 $file = fopen("visitas.txt", "a");
-fwrite($file, date('Y-m-d H:i') . PHP_EOL);
+fwrite($file, date('Y-m-d H:i '));
 fwrite($file, $_SERVER['REMOTE_ADDR'] . PHP_EOL);
 fclose($file);
 
