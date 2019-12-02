1d4a243952d96e33b1d2ebcadc2440f84920461d
Mario Alonso Gutiérrez <marrioalonso31info@gmail.com>
2019-12-02 10:54:17 +0100

    html

--- Tema 3/html.php
+++ Tema 3/html.php
@@ -1,8 +1,47 @@
 <html>
 <head>
-<title>Todo sobre VideoJuegos</title>
-<h1>VIDEOJUEGOS</h1>
+<center><title>Todo sobre VideoJuegos</title>
+<h1><u>VIDEOJUEGOS</u></h1>
+<p><font size=5><b>En esta página vamos a hablar sobre VideoJuegos, en este caso, sobre el Rocket League, League Of Legends y el Counter-Strike.<b/></p>
+
 </head>
-<body>
+<body bgcolor="#04F5F5">
+<center><h2>Rocket League</h2></center>
+<p><font size=5><b>El Rocket League es un juego basado en coches los cuales con cierta destreza puedes conseguir volar con ellos y el objetivo del juego consiste en meter goles.<b/></p>
+<center><img src="RL.jpg" height="420" width="450"></center>
+<center><h3>League of Legends</h3></center>
+<p><font size=5><b>El League Of Legends es el juego gratuito más jugado del mundo en la Historia de los videojuegos. El juego consiste en defender tu base y destruir la base del enemigo.<b/></p>
+<center><img src="LOL.jpg" height="420" width="450"></center>
+<center><h4>Counter Strike</h4></center>
+<p><font size=5><b>El Counter-Strike se basa en un Shooter en el cual se necesita muchos reflejos y grandes habilidades con el ratón a la hora de apuntar.<b/></p>
+<center><img src="CSGO.png" height="420" width="450"></center>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<center><table border="3" ></center>
+  <tr>
+    <th>Provincias bañadas por el cantabrico</th>
+  </tr>
+  <tr>
+    <td>Pais Vasco</td>
+  </tr>
+  <tr>
+    <td>Cantabria</td>
+  </tr>
+    <tr>
+    <td>Asturias</td>
+  </tr>
+</table>
+<br></br>
+<br></br>
+<br></br>
+  <form>
+  Nombre:<br>
+  <input type="text" name="firstname"><br>
+  Apellidos:<br>
+  <input type="text" name="lastname">
+  <p><input type="submit" value="Submit"></p>
+</form>
 </body>
 </html>
