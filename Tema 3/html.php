c57af903712166db1507b13433ae312bfa38aa54
Mario Alonso Gutiérrez <marrioalonso31info@gmail.com>
2019-12-04 09:34:01 +0100

    html

--- Tema 3/html.php
+++ Tema 3/html.php
@@ -1,47 +1,51 @@
 <html>
 <head>
-<center><title>Todo sobre VideoJuegos</title>
-<h1><u>VIDEOJUEGOS</u></h1>
-<p><font size=5><b>En esta página vamos a hablar sobre VideoJuegos, en este caso, sobre el Rocket League, League Of Legends y el Counter-Strike.<b/></p>
-
+<title>Todo sobre VideoJuegos</title>
 </head>
 <body bgcolor="#04F5F5">
+<center><table width="60"></center>
+<tr>
+<td>
+<h1><u>VIDEOJUEGOS</u></h1>
+<p><font size=3><b>En esta página vamos a hablar sobre VideoJuegos, en este caso, sobre el Rocket League, League Of Legends y el Counter-Strike.<b/></p>
 <center><h2>Rocket League</h2></center>
-<p><font size=5><b>El Rocket League es un juego basado en coches los cuales con cierta destreza puedes conseguir volar con ellos y el objetivo del juego consiste en meter goles.<b/></p>
+<p><font size=5 color=#F203FE><b>El Rocket League es un juego basado en coches los cuales con cierta destreza puedes conseguir volar con ellos y el objetivo del juego consiste en meter goles.<b/></p>
 <center><img src="RL.jpg" height="420" width="450"></center>
-<center><h3>League of Legends</h3></center>
-<p><font size=5><b>El League Of Legends es el juego gratuito más jugado del mundo en la Historia de los videojuegos. El juego consiste en defender tu base y destruir la base del enemigo.<b/></p>
+<center><h2>League of Legends</h2></center>
+<p><font size=7 color=#FFFBFF><b>El League Of Legends es el juego gratuito más jugado del mundo en la Historia de los videojuegos. El juego consiste en defender tu base y destruir la base del enemigo.<b/></p>
 <center><img src="LOL.jpg" height="420" width="450"></center>
-<center><h4>Counter Strike</h4></center>
-<p><font size=5><b>El Counter-Strike se basa en un Shooter en el cual se necesita muchos reflejos y grandes habilidades con el ratón a la hora de apuntar.<b/></p>
+<center><h2>Counter Strike</h2></center>
+<p><font size=6 color=#5BFA06><b>El Counter-Strike se basa en un Shooter en el cual se necesita muchos reflejos y grandes habilidades con el ratón a la hora de apuntar.<b/></p>
 <center><img src="CSGO.png" height="420" width="450"></center>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
+<br>
+<br>
+<br>
+<br>
 <center><table border="3" ></center>
   <tr>
-    <th>Provincias bañadas por el cantabrico</th>
+    <th>Comunidades Autónomas</th><th>Provincia</th>
   </tr>
   <tr>
-    <td>Pais Vasco</td>
+    <td>Pais Vasco</td><td>Vizcaya</td>
   </tr>
   <tr>
-    <td>Cantabria</td>
+    <td>Cantabria</td><td>Santander</td>
   </tr>
     <tr>
-    <td>Asturias</td>
+    <td>Principado de Asturias</td><td>Asturias</td>
   </tr>
 </table>
-<br></br>
-<br></br>
-<br></br>
+<br>
+<br>
+<br>
   <form>
   Nombre:<br>
   <input type="text" name="firstname"><br>
   Apellidos:<br>
   <input type="text" name="lastname">
   <p><input type="submit" value="Submit"></p>
+  </td>
+  </table>
 </form>
 </body>
 </html>
